57e59f39c2f79497e8055a376b5c604ead115ec2
samad3911-creator <69162020+samad3911-creator@users.noreply.github.com>
2020-09-10 17:45:21 +0530

    Add files via upload

---
+++ Day4/Problem1/index.js
@@ -0,0 +1,14 @@
+function changeImage1(){
+    const ele=document.getElementById("image");
+    ele.src="light.jpeg"
+}
+
+function changeImage2(){
+    const ele=document.getElementById("image");
+    ele.src="Galaxy.jpeg"
+}
+
+function changeImage3(){
+    const ele=document.getElementById("image");
+    ele.src="l.png"
+}
